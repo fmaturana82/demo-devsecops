472101bcfb8709249a8c8eb8592064f084e12e04
fmaturana <f.amaturana.i@gmail.com>
2022-06-11 19:33:41 -0400

    initial commit

---
+++ src/test/java/com/devsecops/demodevsecops/AppDevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsecops.demodevsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDevsecopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
