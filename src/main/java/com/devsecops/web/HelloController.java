9024f4647caf7376c6dec9307c9f5aea914bef17
fmaturana <f.amaturana.i@gmail.com>
2022-06-11 20:25:10 -0400

    added test class

--- src/main/java/com/devsecops/web/HelloController.java
+++ src/main/java/com/devsecops/web/HelloController.java
@@ -1,7 +1,12 @@
 package com.devsecops.web;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class HelloController {
